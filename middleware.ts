cb7cfc45fde66e9cd81aadec1b6d08c43588fdea
Dennis McMahon <mcmdennis@gmail.com>
2026-01-08 04:33:09 +1000

    adding back middleware

---
+++ middleware.ts
@@ -0,0 +1,46 @@
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: any) {
+          response.cookies.set({ name, value, ...options })
+        },
+        remove(name: string, options: any) {
+          response.cookies.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
+
+  // This call is NOT optional
+  // It hydrates the session for server components
+  await supabase.auth.getSession()
+
+  return response
+}
+
+export const config = {
+  matcher: [
+    /*
+      Run on everything EXCEPT:
+      - static files
+      - _next
+      - favicon
+    */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+}
